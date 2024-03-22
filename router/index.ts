a704a88dce0fd4999fe25c1f79e9714f7a3fb9b0
hernanPoblete <hernan.poblete@ug.uchile.cl>
2024-03-22 09:34:51 -0300

    eliminacion de console.log de prueba

--- router/index.ts
+++ router/index.ts
@@ -3,7 +3,6 @@ import {Router} from 'express';
 let router = Router();
 
 router.get("/", (req, res)=>{
-    console.log(req.cookies);
     req.cookies.session?res.redirect("perfil"):res.render("login.ejs");
 })
 
